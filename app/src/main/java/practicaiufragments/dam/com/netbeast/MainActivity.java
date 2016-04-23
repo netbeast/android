4b5cb0cbead7f27a18444490b08d38948b86a0d8
franrios <fcojriosbello@gmail.com>
2016-04-23 13:47:32 +0200

    resources added to general layout

--- app/src/main/java/practicaiufragments/dam/com/netbeast/MainActivity.java
+++ app/src/main/java/practicaiufragments/dam/com/netbeast/MainActivity.java
@@ -14,9 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.general_activity);
     }
 
     @Override
