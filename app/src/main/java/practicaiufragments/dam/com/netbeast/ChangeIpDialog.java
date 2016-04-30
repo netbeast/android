7deb0af53c42120d3a26de2b8d01c7fe1e1ac29c
franrios <fcojriosbello@gmail.com>
2016-04-30 12:20:13 +0200

    added validation function and dependencies

--- app/src/main/java/practicaiufragments/dam/com/netbeast/ChangeIpDialog.java
+++ app/src/main/java/practicaiufragments/dam/com/netbeast/ChangeIpDialog.java
@@ -1,22 +1,35 @@
 package practicaiufragments.dam.com.netbeast;
 
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Alejandro Rodríguez Calzado on 28/04/16.
  */
 public class ChangeIpDialog extends DialogFragment {
     View customView;
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -29,11 +42,15 @@ public class ChangeIpDialog extends DialogFragment {
         builder.setView(customView)
                 // Add action buttons
                 .setPositiveButton(R.string.connect_dialog_button, new DialogInterface.OnClickListener() {
+                    @TargetApi(Build.VERSION_CODES.M)
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        EditText ip = (EditText) customView.findViewById(R.id.ipTextEdit);
 
-                        if (ip.getText().toString().isEmpty()) {
+                        EditText ip = (EditText) customView.findViewById(R.id.ipTextEdit);
+                        String givenIP = ip.getText().toString();
+                        // givenIP is the one that de user set so we need to verify the format
+
+                        if (givenIP.isEmpty() || !validateIP(givenIP)) {
                             Toast.makeText(getContext(),
                                     R.string.ip_error_dialog,
                                     Toast.LENGTH_LONG).show();
@@ -61,5 +78,12 @@ public class ChangeIpDialog extends DialogFragment {
         super.onStart();
         ((AlertDialog) getDialog()).getWindow().setBackgroundDrawableResource(R.color.background);
 
+    }
+
+    public boolean validateIP(String ip){
+        pattern = Pattern.compile(IPADDRESS_PATTERN);
+        matcher = pattern.matcher(ip);
+        Log.d("validator", "Value: " + matcher.matches());
+        return matcher.matches();
     }
 }
