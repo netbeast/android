7c72118c74bbabbc7a16c922ff216e19a764bc91
Cayetano Rodríguez Medina <cayetanorodriguez94@gmail.com>
2016-05-19 20:31:52 +0200

    Added splashscreen

---
+++ app/src/main/java/practicaiufragments/dam/com/netbeast/Splash.java
@@ -0,0 +1,34 @@
+package practicaiufragments.dam.com.netbeast;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Cayetano Rodríguez Medina on 19/5/16.
+ */
+
+public class Splash extends Activity {
+
+    // Duration of wait
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash_layout);
+
+        // New Handler to start the Main Activity
+        // and close this Splash-Screen after some seconds.
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                // Create an Intent that will start the Main Activity.
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
